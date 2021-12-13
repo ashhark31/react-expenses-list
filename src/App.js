0abb01886bf485ba3778cfc9d5f30a4246c369c1
Mohammad Ashhar khan <ashhark31@gmail.com>
2021-12-13 17:10:55 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
+import NewExpense from './components/NewExpense/NewExpense'
+import Expenses from './components/Expenses/Expenses'
 
-function App() {
-  return (
-    <div className="App">
+const dummy_expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      id: 'e2',
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+];
+/*  <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +45,23 @@ function App() {
           Learn React
         </a>
       </header>
+  </div>*/
+
+const App = () => {
+  const [expenses, setExpenses] =  useState(dummy_expenses)
+
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
+  }
+
+  //return React.createElement('div',{}, React.createElement(Expenses, {items: expenses}));
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
